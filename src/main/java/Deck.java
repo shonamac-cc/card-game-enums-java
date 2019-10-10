0d4642c37e35ba56cef1f4c60046d3c71d6cc8b2
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-10-10 13:59:48 +0100

    populate deck with 52 cards

---
+++ src/main/java/Deck.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Deck {
+
+    private ArrayList<Card> cards;
+
+    public Deck() {
+        this.cards = new ArrayList<Card>();
+    }
+
+    public int countCards() {
+        return this.cards.size();
+    }
+
+    public void addCard(Card card) {
+        this.cards.add(card);
+    }
+
+    public void add52Cards() {
+        for (SuitType s : SuitType.values()) {
+            for (RankType r : RankType.values()) {
+//               ArrayList<Card>[i] = new Card(RankType r, SuitType s);
+//                this.cards.add(r, s);
+//                ArrayList<Card>[i] = new Card(r, s);
+//                i++;
+                Card card = new Card(s,r);
+                this.addCard(card);
+            }
+        }
+
+    }
+}
+
---
+++ src/test/java/DeckTest.java
@@ -0,0 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeckTest {
+
+    private Deck deck;
+    private Card card;
+
+    @Before
+    public void before(){
+        deck = new Deck();
+        card = new Card(SuitType.SPADES, RankType.ACE);
+    }
+
+    @Test
+    public void deckStartsEmpty(){
+        assertEquals(0, deck.countCards());
+    }
+
+    @Test
+    public void canAddOneCard() {
+        deck.addCard(card);
+        assertEquals(1, deck.countCards());
+    }
+
+    @Test
+    public void canAdd52Cards() {
+        deck.add52Cards();
+        assertEquals(52, deck.countCards());
+    }
+
+}
